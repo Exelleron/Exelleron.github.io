712f0bdafb7604d7d54d548feeee5ab1c24e68f7
Exelleron <exelleron1@gmail.com>
2022-02-17 15:39:00 +0200

    'non-working-filter'

--- js/main.js
+++ js/main.js
@@ -80,10 +80,42 @@ function removeModalCard(event) {
     }
 }
 
-items.map(item => {
-    const element = document.createElement('div');
-    const card = () => {
-        return (`<div class="card_like">
+document.querySelectorAll('.filter input').forEach(elem => {
+    elem.addEventListener('change', function () {
+        const filter = {display: [], memory: [], color: [], os:[], price: {min: 0, max: 10000}};
+
+        document.querySelectorAll('.filter_display input:checked').forEach(elem => {
+            filter.display.push(elem.value)
+        });
+        document.querySelectorAll('.filter_memory input:checked').forEach(elem => {
+            filter.memory.push(elem.value)
+        });
+        document.querySelectorAll('.filter_color input:checked').forEach(elem => {
+            filter.color.push(elem.value)
+        });
+        document.querySelectorAll('.filter_os input:checked').forEach(elem => {
+            filter.os.push(elem.value)
+        });
+        filter.price.min = document.querySelector('.filter_price--min').value;
+        items.map(item => {
+            if (filter.memory.length && !filter.memory.includes(item.storage)){
+                return
+            }
+            if (filter.color.length && !filter.color.includes(item.color)){
+                return
+            }
+            if (filter.display.length && !filter.display.includes(item.display)){
+                return
+            }
+            if (filter.os.length && !filter.os.includes(item.os)){
+                return
+            }
+            if (filter.price.min > item.price){
+                return
+            }
+            const element = document.createElement('div');
+            const card = () => {
+                return (`<div class="card_like">
                         <img src="images/like.svg" alt="like">
                     </div>
 
@@ -139,12 +171,15 @@ items.map(item => {
                             </p>
                         </div>
                     </div>`)
-    };
-    element.onclick = () => innerModalCard(item);
-    element.classList.add('card');
-    element.insertAdjacentHTML('beforeend', card());
-    itemsContainer.insertAdjacentElement('beforeend', element);
+            };
+            element.onclick = () => innerModalCard(item);
+            element.classList.add('card');
+            element.insertAdjacentHTML('beforeend', card());
+            itemsContainer.insertAdjacentElement('beforeend', element);
+        });
+    })
 });
+
 
 document.querySelector('.main_search-bar--filter').addEventListener('click', function () {
     document.querySelector('.filter').classList.toggle('open');
@@ -158,89 +193,44 @@ document.querySelectorAll('.filter_arrow').forEach(elem => {
     })
 });
 
-// document.querySelectorAll('.filter input').forEach(elem => {
-//     elem.addEventListener('change', function () {
-//         const filter = {memory: [], color: [], os:[], price: {min: 0, max: 10000}};
+
+
+// const filter = {
+//     display:[]
+// };
+
+// function filterUpdate(key,value){
+//     console.log(key,value.srcElement.defaultValue)
+//     if(value.srcElement.checked){
+//         let index = filter[key].find(filterValue => filterValue.id === value.srcElement.defaultValue)
+//         if(!index){
+//             filter[key].push({id:value.srcElement.defaultValue, from :value.srcElement.defaultValue.split("-")[0],to :value.srcElement.defaultValue.split("-")[1]});
+//         } }else{
+//         let index = filter[key].findIndex(filterValue => filterValue.id === value.srcElement.defaultValue)
+//         if(index>-1){
+//             filter[key].splice(index);
+//         }
 //
-//         document.querySelectorAll('.filter_memory input:checked').forEach(elem => {
-//             filter.memory.push(elem.value)
-//         });
-//         document.querySelectorAll('.filter_color input:checked').forEach(elem => {
-//             filter.color.push(elem.value)
-//         });
-//         filter.price.min = document.querySelector('.filter_price--min').value
-//         items.map(item => {
-//             if (filter.memory.length && !filter.memory.includes(item.storage)){
-//                 return
-//             }
-//             if (filter.color.length && !filter.color.includes(item.color)){
-//                 return
-//             }
-//             if (filter.price.min > item.price){
-//                 return
-//             }
+//     }
+//     if(filter[key].length===0){
+//         createCard(items)
+//         return;
+//     }
+//     runFilter()
+// }
 //
-//             const card = `<div class="card">
-//                     <div class="card_like">
-//                         <img src="images/like.svg" alt="like">
-//                     </div>
+// function runFilter(){
 //
-//                     <div class="card_image">
-//                         <img
-//                             class="main-image"
-//                             src="${item.imgUrl}"
-//                             alt="product_image"
-//                         >
-//                     </div>
+//     let filterObjects = items.filter(device => filteredData(device));
+//     cardsContainer.innerHTML = "";
+//     createCard(filterObjects)
+// }
 //
-//                     <div class="card_header">
-//                         <span class="card_header--title">${item.name}</span>
-//                     </div>
 //
-//                     <div class="card_info">
-//                         <p class="card_info--stock">
-//                             <img
-//                                 class="card_info--true-false"
-//                                 src="${item.orderInfo.inStock ? 'images/true.svg' : 'images/false.svg'}"
-//                                 alt="true"
-//                             >
-//                             <span class="count-in-stock">${item.orderInfo.inStock}</span> left in stock
-//                         </p>
-//                         <p class="card_info--price">
-//                             Price: <strong>${item.price}</strong> $
-//                         </p>
-//                     </div>
-//
-//                     <button class="card_button button ${!item.orderInfo.inStock ? 'gray-button' : ''}">
-//                         Add to cart
-//                     </button>
-//
-//                     <div class="card_footer">
-//                         <div>
-//                             <img
-//                                 src="images/red-like.svg"
-//                                 alt=""
-//                                 class="card_footer--red-like"
-//                             >
-//                             <p class="card_footer--reviews">
-//                                 <strong>${item.orderInfo.reviews}%</strong>
-//                                 Positive reviews
-//                                 <br>
-//                                 Above avarage
-//                             </p>
-//                         </div>
-//                         <div>
-//                             <p class="card_footer--orders">
-//                                 <strong>${Math.round(0.5 + Math.random() * 10000)}</strong>
-//                                 <br>
-//                                 orders
-//                             </p>
-//                         </div>
-//                     </div>
-//                 </div>`;
-//
-//             itemsContainer.innerHTML += card;
-//
-//         });
-//     })
-// });
+// function filteredData(device){
+//     let filterDevice = filter.display;
+//     let res = filterDevice.find(filterItem=>  device.display > filterItem.from && device.display < filterItem.to)
+// //let allKeys =filter.keys()
+//     console.log(res);
+//     return !!res
+// }
